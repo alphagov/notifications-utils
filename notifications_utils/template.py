166ddd6982d98b5b87b51dd3af7ce8c1e4739ed9
Chris Hill-Scott <me@quis.cc>
2017-07-24 23:11:56 +0100

    Reformat for readability
    Don’t need the extra brackets

--- notifications_utils/template.py
+++ notifications_utils/template.py
@@ -139,10 +139,10 @@ class SMSMessageTemplate(Template):
 
     @property
     def content_count(self):
-        return len((
+        return len(
             # we always want to call SMSMessageTemplate.__str__ regardless of subclass, to avoid any html formatting
-            SMSMessageTemplate.__str__(self)
-        ).encode(self.encoding))
+            SMSMessageTemplate.__str__(self).encode(self.encoding)
+        )
 
     @property
     def fragment_count(self):
