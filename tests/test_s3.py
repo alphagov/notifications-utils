094e4059f155a4af606eb66ac11c8e2da988f8fa
Chris Hill-Scott <me@quis.cc>
2018-05-15 16:26:44 +0100

    Remove auto bucket creation from S3 client
    It never has permission to do this stuff anyway.

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -42,25 +42,11 @@ def test_s3upload_save_file_to_bucket_with_contenttype(mocker):
     )
 
 
-def test_s3upload_creates_bucket_if_bucket_does_not_exist(mocker):
-    mocked = mocker.patch('notifications_utils.s3.resource')
-    response = {'Error': {'Code': 404}}
-    exception = botocore.exceptions.ClientError(response, 'Does not exist')
-    mocked.return_value.meta.client.head_bucket.side_effect = exception
-    s3upload(filedata=contents,
-             region=region,
-             bucket_name=bucket,
-             file_location=location)
-    mocked_create_bucket = mocked.return_value.create_bucket
-    mocked_create_bucket.assert_called_once_with(Bucket=bucket,
-                                                 CreateBucketConfiguration={'LocationConstraint': region})
-
-
 def test_s3upload_raises_exception(app, mocker):
     mocked = mocker.patch('notifications_utils.s3.resource')
     response = {'Error': {'Code': 500}}
     exception = botocore.exceptions.ClientError(response, 'Bad exception')
-    mocked.return_value.meta.client.head_bucket.side_effect = exception
+    mocked.return_value.Object.return_value.put.side_effect = exception
     with pytest.raises(botocore.exceptions.ClientError):
         s3upload(filedata=contents,
                  region=region,
