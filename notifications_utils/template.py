785e4c891a637900f6a713ac45b26bdf010493c9
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2020-07-09 19:28:23 +0100

    allow you to create broadcast message template with areas
    rather than forcing you to provide raw polygons and giving you the
    opportunity to screw up and give long lat vs lat long

--- notifications_utils/template.py
+++ notifications_utils/template.py
@@ -40,6 +40,7 @@ from notifications_utils.countries.data import Postage
 from notifications_utils.take import Take
 from notifications_utils.template_change import TemplateChange
 from notifications_utils.sanitise_text import SanitiseSMS
+from notifications_utils.broadcast_areas import broadcast_area_libraries
 
 
 template_env = Environment(loader=FileSystemLoader(
@@ -372,10 +373,16 @@ class BroadcastMessageTemplate(BaseBroadcastTemplate, SMSMessageTemplate):
         template,
         values=None,
         polygons=None,
+        areas=None,
         identifier='',
     ):
         super().__init__(template, values)
-        self._polygons = polygons or []
+
+        if areas is not None:
+            self._polygons = broadcast_area_libraries.get_polygons_for_areas_lat_long(areas)
+        else:
+            self._polygons = polygons or []
+
         self.sent = datetime.utcnow()
         self.expires = datetime.utcnow() + timedelta(hours=self.default_ttl_hours)
         self.notify_identifier = 'https://www.notifications.service.gov.uk/'
