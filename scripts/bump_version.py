715d311e7a03b377b028a0a3c5b98c99b59a224e
Chris Hill-Scott <me@quis.cc>
2021-11-30 11:19:32 +0000

    Add some helpful print statements
    We want to encourage people to update the changelog when releasing a
    new version.
    
    This commit not only adds a prompt telling them to do so, it makes the
    process easier by giving them a template to copy.

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -42,3 +42,50 @@ __version__ = '{new_major}.{new_minor}.{new_patch}'  # {package_contents_hash}
 
 with Path("notifications_utils/version.py").open("w") as version_file:
     version_file.write(output)
+
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
+print('')
+print(
+    f'{color.BOLD}{color.GREEN}'
+    f'Changed version from {__version__} to {new_major}.{new_minor}.{new_patch}'
+    f'{color.END} ✅'
+)
+print('')
+print('   Update requirements files in other apps with:')
+print('')
+print(
+    f'   '
+    f'notifications-utils @ git+https://github.com/alphagov/notifications-utils.git'
+    f'@{new_major}.{new_minor}.{new_patch}'
+)
+print('')
+
+if version_part == 'minor':
+    print(f'{color.YELLOW}{color.BOLD}Consider updating CHANGELOG.md, for example:{color.END}')
+    print(f'''
+    ## {new_major}.{new_minor}.{new_patch}
+
+    * Details of change 1
+    * Details of change 2
+    ''')
+
+if version_part == 'major':
+    print(f'{color.YELLOW}{color.BOLD}Make sure to update CHANGELOG.md, for example:{color.END}')
+    print(f'''
+    ## {new_major}.{new_minor}.{new_patch}
+
+    * Details of breaking change
+    ''')
