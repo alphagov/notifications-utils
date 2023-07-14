5852a752f737ec24236fc0192b6f2139b2344a28
Tom Byers <tombaromba@gmail.com>
2023-07-14 11:05:08 +0100

    Fix HTML5 validation errors
    Includes:
    - removal of trailing slash for void elements like
      <meta>, <img> or <br>
    - removal of type="text/css" attribute for <style>
      elements

--- notifications_utils/markdown.py
+++ notifications_utils/markdown.py
@@ -182,7 +182,7 @@ class NotifyEmailMarkdownRenderer(NotifyLetterMarkdownPreviewRenderer):
         return '<hr style="border: 0; height: 1px; background: #B1B4B6; Margin: 30px 0 30px 0;">'
 
     def linebreak(self):
-        return "<br />"
+        return "<br>"
 
     def list(self, body, ordered=True):
         return (
