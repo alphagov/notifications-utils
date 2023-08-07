4f01859678393ad830628402cacaf54e325b8654
Chris Hill-Scott <me@quis.cc>
2023-08-07 15:28:58 +0100

    Style long QR code placeholder text better
    When a QR code contains a placeholder we render a ‘placeholder’ box
    instead, which has a dashed border.
    
    If the text of the placeholder or its prefix/suffix is long, then this
    causes the text to wrap inside the placeholder box. Because the
    placeholder box is a relatively narrow fixed width this:
    - happens easily
    - does not look very good
    - makes the text harder to read
    
    This commit adds some extra HTML elements which allow us to style the
    box and its contents separately.
    
    This way the contents can overflow the box without looking messy. The
    width of the text is therefore limited to the width of the column, as it
    is for other URLs and placeholders.
    
    Ideally this would use vertical-align: middle to centre the text, even
    when it spans multiple lines. But Weasyprint doesn’t seem to support
    this. Instead we use using a negative margin to vertically centre one
    line of text, which will have to do.

--- tests/test_template_types.py
+++ tests/test_template_types.py
@@ -3136,24 +3136,42 @@ def test_letter_image_template_marks_first_page_of_attachment():
         (
             LetterPreviewTemplate,
             {"template_type": "letter", "subject": "foo", "content": "[QR](((var)))"},
-            "<p><div class='qrcode‑placeholder'><span class='placeholder'>&#40;&#40;var&#41;&#41;</span></div></p>",
+            (
+                "<p>\n"
+                "<div class='qrcode-placeholder'>\n"
+                "    <div class='qrcode-placeholder-border'></div>\n"
+                "    <div class='qrcode-placeholder-content'>\n"
+                "        <span class='qrcode-placeholder-content-background'><span class='placeholder'>&#40;&#40;var&#41;&#41;</span></span>\n"  # noqa
+                "    </div>\n"
+                "</div>\n"
+                "</p>"
+            ),
         ),
         (
             LetterPreviewTemplate,
             {"template_type": "letter", "subject": "foo", "content": "[QR](https://blah.blah/?query=((var)))"},
             (
-                "<p><div class='qrcode‑placeholder'>"
-                "https://blah.blah/?query=<span class='placeholder'>&#40;&#40;var&#41;&#41;</span>"
-                "</div></p>"
+                "<p>\n"
+                "<div class='qrcode-placeholder'>\n"
+                "    <div class='qrcode-placeholder-border'></div>\n"
+                "    <div class='qrcode-placeholder-content'>\n"
+                "        <span class='qrcode-placeholder-content-background'>https://blah.blah/?query=<span class='placeholder'>&#40;&#40;var&#41;&#41;</span></span>\n"  # noqa
+                "    </div>\n"
+                "</div>\n"
+                "</p>"
             ),
         ),
         (
             LetterPreviewTemplate,
             {"template_type": "letter", "subject": "foo", "content": "[QR](pre((var))post)"},
             (
-                "<p><div class='qrcode‑placeholder'>"
-                "pre<span class='placeholder'>&#40;&#40;var&#41;&#41;</span>post"
-                "</div></p>"
+                "<p>\n<div class='qrcode-placeholder'>\n"
+                "    <div class='qrcode-placeholder-border'></div>\n"
+                "    <div class='qrcode-placeholder-content'>\n"
+                "        <span class='qrcode-placeholder-content-background'>pre<span class='placeholder'>&#40;&#40;var&#41;&#41;</span>post</span>\n"  # noqa
+                "    </div>\n"
+                "</div>\n"
+                "</p>"
             ),
         ),
         (
