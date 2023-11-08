a4b589aaf3946f7e1905b0b184ca5d5aeec19684
Chris Hill-Scott <me@quis.cc>
2023-11-08 10:38:43 +0000

    Patch version bump to 72.1.1

--- notifications_utils/version.py
+++ notifications_utils/version.py
@@ -5,4 +5,4 @@
 # - `make version-minor` for new features
 # - `make version-patch` for bug fixes
 
-__version__ = "70.0.7"  # 3951650faacc092d58042571d8fc4024
+__version__ = "72.1.1"  # 3951650faacc092d58042571d8fc4024
