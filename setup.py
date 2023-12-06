2550db7ec2a50f80d1dca8cd3132efd702f64a0a
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2023-12-06 18:10:31 +0000

    set required flask version in reqs_for_test
    we support 2.x (and still use it on a variety of apps) but the tests
    specifically patch `flask.sansio.app.create_logger` to test that logging
    works as expected. So for testing, you need a higher version of flask.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         "mistune<2.0.0",  # v2 is totally incompatible with unclear benefit
         "requests>=2.25.0",
         "python-json-logger>=2.0.1",
-        "Flask>=1.1.1",
+        "Flask>=2.1.1",
         "orderedset>=2.0.3",
         "Jinja2>=2.11.3",
         "statsd>=3.3.0",
