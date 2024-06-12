7f44e05b6354c2f4cec6040b2bb50524203972d2
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-06-12 12:12:05 +0100

    Move InvalidPhoneError messages to a dict within the exception obj
    trialling a new more explicit way to manage our error strings.
    
    We have issues managing our error messages, where we've got a bunch of
    highly tailored messages that were initially designed for showing errors
    on the frontend, but are also sent via the API. We know that some teams
    are checking these error message strings for specific content as they
    don't have any other way to refer to specific errors that they might
    want to bubble up on their own frontends etc.
    
    This work changes that:
    
    * Require a `code` field when creating/raising an `InvalidPhoneError`.
    * Define all error messages in `InvalidPhoneError.ERROR_MESSAGES`. This
      should be the source of truth. If a specific place needs error
      messages, then it probably makes sense to create a separate dict in
      InvalidPhoneError so that all the varying content is in one location.
    * If the code doesn't have a matching error message, then a KeyError
      will be raised, so pay attention to your exceptions carefully to make
      sure it's set up correctly.
    
    We may end up with helper functions or additional error message mapping
    dicts (for example, to handle frontend vs api error messages,
    international vs uk-only, landline vs mobile, a user's own number vs a
    recipient number). For now, lets try and keep all these in
    `InvalidPhoneError`. This will make it easier to reason about what error
    messages there are so we can be sure that they're all kept as up-to-date
    as possible

--- notifications_utils/recipient_validation/errors.py
+++ notifications_utils/recipient_validation/errors.py
@@ -1,7 +1,15 @@
+from enum import StrEnum, auto
+
+
 class InvalidRecipientError(Exception):
     message = "Not a valid recipient address"
+    code = "UNKNOWN"
 
-    def __init__(self, message=None):
+    def __init__(self, message: str = None, code: str = None):
+        if code:
+            self.code = code
+            if not message:
+                message = code
         super().__init__(message or self.message)
 
 
@@ -10,7 +18,32 @@ class InvalidEmailError(InvalidRecipientError):
 
 
 class InvalidPhoneError(InvalidRecipientError):
-    message = "Not a valid phone number"
+    class Codes(StrEnum):
+        INVALID_NUMBER = auto()
+        TOO_LONG = auto()
+        TOO_SHORT = auto()
+        NOT_A_UK_MOBILE = auto()
+        UNKNOWN_CHARACTER = auto()
+        UNSUPPORTED_COUNTRY_CODE = auto()
+
+    # TODO: Move this somewhere else maybe? Maybe not?
+    ERROR_MESSAGES = {
+        Codes.INVALID_NUMBER: "Not a valid phone number",
+        Codes.TOO_LONG: "Mobile number is too long",
+        Codes.TOO_SHORT: "Mobile number is too short",
+        Codes.NOT_A_UK_MOBILE: (
+            "This does not look like a UK mobile number – double check the mobile number you entered"
+        ),
+        Codes.UNKNOWN_CHARACTER: "Mobile numbers can only include: 0 1 2 3 4 5 6 7 8 9 ( ) + -",
+        Codes.UNSUPPORTED_COUNTRY_CODE: "Country code not found - double check the mobile number you entered",
+    }
+
+    def __init__(self, *, code: Codes = Codes.INVALID_NUMBER):
+        """
+        Create an InvalidPhoneError. The code must be present in InvalidPhoneError.ERROR_MESSAGES or this will raise a
+        KeyError which you may not expect!
+        """
+        super().__init__(code=code, message=self.ERROR_MESSAGES[code])
 
 
 class InvalidAddressError(InvalidRecipientError):
