d2676e0d19bc0b67666e9f643d6be2bcdf16cd86
Chris Hill-Scott <me@quis.cc>
2024-06-26 11:19:20 +0100

    Copy common test requirements into apps
    So that apps can automatically share the same dependencies, and when we
    want to upgrade a new dependency we only have to do it in one place.

--- notifications_utils/version_tools.py
+++ notifications_utils/version_tools.py
@@ -4,8 +4,11 @@ import requests
 
 requirements_file = pathlib.Path("requirements.in")
 frozen_requirements_file = pathlib.Path("requirements.txt")
-pyproject_file = pathlib.Path("pyproject.toml")
 repo_name = "alphagov/notifications-utils"
+config_files = {
+    "pyproject.toml",
+    "requirements_for_test_common.txt",
+}
 
 
 class color:
@@ -82,7 +85,8 @@ def get_file_contents_from_github(branch_or_tag, path):
 
 def copy_config():
     local_utils_version = get_app_version()
-    remote_contents = get_file_contents_from_github(local_utils_version, "pyproject.toml")
-    pyproject_file.write_text(
-        f"# This file is automatically copied from notifications-utils@{local_utils_version}\n\n{remote_contents}"
-    )
+    for config_file in config_files:
+        remote_contents = get_file_contents_from_github(local_utils_version, config_file)
+        pathlib.Path(config_file).write_text(
+            f"# This file is automatically copied from notifications-utils@{local_utils_version}\n\n{remote_contents}"
+        )
