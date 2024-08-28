b492d7d748ad928f05ae4b4d5e38fe1db7b8e377
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2024-08-28 14:17:06 +0100

    TMP LazyLocalGetterResetter

--- notifications_utils/local_vars.py
+++ notifications_utils/local_vars.py
@@ -32,3 +32,32 @@ class LazyLocalGetter(Generic[T]):
 
     def clear(self) -> None:
         self.context_var.set(None)
+
+
+class LazyLocalGetterResetter:
+    """
+    Holds a list of LazyLocalGetter instances and allows them to be reset
+    to their "original" state in a single call.
+
+    In reality this is only ever used for resetting between tests
+    """
+    _getters: list[LazyLocalGetter]
+
+    def __init__(self):
+        self._getters = []
+
+    def register(self, getter: LazyLocalGetter) -> None:
+        """
+        Adds the supplied getter to the list of instances to reset, making
+        a note of its current `factory` to be restored on reset.
+        """
+        self._getters.append((getter, getter.factory,))
+
+    def reset(self) -> None:
+        """
+        Resets the registered LazyLocalGetters and restores their factory
+        function to the one at time of registration.
+        """
+        for getter, factory in self._getters:
+            getter.factory = factory
+            getter.clear()
