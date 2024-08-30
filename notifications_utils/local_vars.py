b8b414fea4969434dd6249560b24e683b0bafe6d
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2024-08-30 16:22:28 +0100

    TMP

--- notifications_utils/local_vars.py
+++ notifications_utils/local_vars.py
@@ -12,8 +12,14 @@ class LazyLocalGetter(Generic[T]):
 
     context_var: ContextVar[T | None]
     factory: Callable[[], T]
+    expected_type: type[T] | None
 
-    def __init__(self, context_var: ContextVar[T | None], factory: Callable[[], T]):
+    def __init__(
+        self,
+        context_var: ContextVar[T | None],
+        factory: Callable[[], T],
+        expected_type: type[T] | None=None,
+    ):
         """
         Given a reference to a `context_var`, the resulting instance will be a callable that
         returns the current context's contents of that `context_var`, pre-populating it with
@@ -21,11 +27,19 @@ class LazyLocalGetter(Generic[T]):
         """
         self.context_var = context_var
         self.factory = factory
+        self.expected_type = expected_type
 
     def __call__(self) -> T:
         r = self.context_var.get(None)
         if r is None:
             r = self.factory()
+
+            # exact type testing here, none of your issubclass flexibility
+            if self.expected_type is not None and type(r) is not self.expected_type:
+                raise TypeError(
+                    f"factory returned value (of type {type(r)}) that is not of the expected type {self.expected_type}"
+                )
+
             self.context_var.set(r)
 
         return r
