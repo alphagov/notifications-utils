d4e961a04fef7b58f22ac9f6ad7bfddde09866b2
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2024-09-02 13:36:42 +0100

    Bump version to 82.7.0

--- notifications_utils/version.py
+++ notifications_utils/version.py
@@ -5,4 +5,4 @@
 # - `make version-minor` for new features
 # - `make version-patch` for bug fixes
 
-__version__ = "82.6.1"  # b4df1ae664036399e4b5661f55b2d3e5
+__version__ = "82.7.0"  # b21ed1702bc409f764d6898e729430b7
