68a818ee2e8bf3fac441af224dd9be56962fc6b4
Chris Hill-Scott <me@quis.cc>
2025-01-08 10:43:53 +0000

    Bump minimum jinja2 version to latest
    This fixes a couple of moderate security vulnerabilities.

--- notifications_utils/version.py
+++ notifications_utils/version.py
@@ -5,4 +5,4 @@
 # - `make version-minor` for new features
 # - `make version-patch` for bug fixes
 
-__version__ = "92.1.0"  # 982357932659deadbeef
+__version__ = "92.1.1"  # defeaed2e5fb6beb0e73c116bd6a1375
