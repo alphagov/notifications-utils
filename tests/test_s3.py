168465569eba675fc9e8762d11690da9ce67ad12
spatel033 <177532680+spatel033@users.noreply.github.com>
2025-03-25 18:44:41 +0000

    Add s3 multipart upload
    Add multipart upload for large file handling. Implement full multipart upload lifecycle (create, upload, complete, abort).
    Documentation here: https://docs.aws.amazon.com/AmazonS3/latest/userguide/s3-express-using-multipart-upload.html

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -3,13 +3,23 @@ from urllib.parse import parse_qs
 import botocore
 import pytest
 
-from notifications_utils.s3 import S3ObjectNotFound, s3download, s3upload
+from notifications_utils.s3 import (
+    S3ObjectNotFound,
+    s3_multipart_upload_abort,
+    s3_multipart_upload_complete,
+    s3_multipart_upload_create,
+    s3_multipart_upload_part,
+    s3download,
+    s3upload,
+)
 
 contents = "some file data"
 region = "eu-west-1"
 bucket = "some_bucket"
 location = "some_file_location"
 content_type = "binary/octet-stream"
+upload_id = "test-123-upload-id"
+part_number = 23
 
 
 def test_s3upload_save_file_to_bucket(mocker):
@@ -93,3 +103,133 @@ def test_s3download_raises_on_error(mocker):
 
     with pytest.raises(S3ObjectNotFound):
         s3download("bucket", "location.file")
+
+
+def test_s3_multipart_upload_create(mocker):
+    mocked_s3_client = mocker.patch("notifications_utils.s3.client")
+    mocked_create_multipart_upload = mocked_s3_client.return_value.create_multipart_upload
+
+    s3_multipart_upload_create(bucket_name=bucket, file_location=location)
+
+    mocked_create_multipart_upload.assert_called_once_with(
+        Bucket=bucket,
+        Key=location,
+        ServerSideEncryption="AES256",
+        ContentType=content_type,
+    )
+
+
+def test_s3_multipart_upload_create_with_content_type(mocker):
+    mocked_s3_client = mocker.patch("notifications_utils.s3.client")
+    mocked_create_multipart_upload = mocked_s3_client.return_value.create_multipart_upload
+
+    s3_multipart_upload_create(bucket_name=bucket, file_location=location, content_type="text/csv")
+
+    mocked_create_multipart_upload.assert_called_once_with(
+        Bucket=bucket,
+        Key=location,
+        ServerSideEncryption="AES256",
+        ContentType="text/csv",
+    )
+
+
+def test_s3_multipart_upload_create_failure(mocker, app_with_mocked_logger):
+    response = {"Error": {"Code": 500}}
+    exception = botocore.exceptions.ClientError(response, "Bad exception")
+
+    mocked_s3_client = mocker.patch("notifications_utils.s3.client")
+    mocked_instance = mocked_s3_client.return_value
+    mocked_instance.create_multipart_upload.side_effect = exception
+
+    with pytest.raises(botocore.exceptions.ClientError):
+        s3_multipart_upload_create(bucket_name=bucket, file_location=location)
+
+    mocked_instance.create_multipart_upload.assert_called_once()
+
+
+def test_s3_multipart_upload_part(mocker):
+    mocked_s3_client = mocker.patch("notifications_utils.s3.client")
+    mocked_upload_part = mocked_s3_client.return_value.upload_part
+
+    s3_multipart_upload_part(
+        part_number=part_number, bucket_name=bucket, filename=location, upload_id=upload_id, data_bytes=contents
+    )
+
+    mocked_upload_part.assert_called_once_with(
+        Bucket=bucket,
+        Key=location,
+        PartNumber=part_number,
+        UploadId=upload_id,
+        Body=contents,
+    )
+
+
+def test_s3_multipart_upload_part_failure(mocker, app_with_mocked_logger):
+    response = {"Error": {"Code": 500}}
+    exception = botocore.exceptions.ClientError(response, "Bad exception")
+
+    mocked_s3_client = mocker.patch("notifications_utils.s3.client")
+    mocked_instance = mocked_s3_client.return_value
+    mocked_instance.upload_part.side_effect = exception
+
+    with pytest.raises(botocore.exceptions.ClientError):
+        s3_multipart_upload_part(
+            part_number=part_number, bucket_name=bucket, filename=location, upload_id=upload_id, data_bytes=contents
+        )
+
+    mocked_instance.upload_part.assert_called_once()
+
+
+def test_s3_multipart_upload_complete(mocker):
+    mocked_s3_client = mocker.patch("notifications_utils.s3.client")
+    mocked_complete_multipart_upload = mocked_s3_client.return_value.complete_multipart_upload
+
+    s3_multipart_upload_complete(bucket_name=bucket, filename=location, upload_id=upload_id, parts=[])
+
+    mocked_complete_multipart_upload.assert_called_once_with(
+        Bucket=bucket,
+        Key=location,
+        MultipartUpload={"Parts": []},
+        UploadId=upload_id,
+    )
+
+
+def test_s3_multipart_upload_complete_failure(mocker, app_with_mocked_logger):
+    response = {"Error": {"Code": 500}}
+    exception = botocore.exceptions.ClientError(response, "Bad exception")
+
+    mocked_s3_client = mocker.patch("notifications_utils.s3.client")
+    mocked_instance = mocked_s3_client.return_value
+    mocked_instance.complete_multipart_upload.side_effect = exception
+
+    with pytest.raises(botocore.exceptions.ClientError):
+        s3_multipart_upload_complete(bucket_name=bucket, filename=location, upload_id=upload_id, parts=[])
+
+    mocked_instance.complete_multipart_upload.assert_called_once()
+
+
+def test_s3_multipart_upload_abort(mocker):
+    mocked_s3_client = mocker.patch("notifications_utils.s3.client")
+    mocked_abort_multipart_upload = mocked_s3_client.return_value.abort_multipart_upload
+
+    s3_multipart_upload_abort(bucket_name=bucket, filename=location, upload_id=upload_id)
+
+    mocked_abort_multipart_upload.assert_called_once_with(
+        Bucket=bucket,
+        Key=location,
+        UploadId=upload_id,
+    )
+
+
+def test_s3_multipart_upload_abort_failure(mocker, app_with_mocked_logger):
+    response = {"Error": {"Code": 500}}
+    exception = botocore.exceptions.ClientError(response, "Bad exception")
+
+    mocked_s3_client = mocker.patch("notifications_utils.s3.client")
+    mocked_instance = mocked_s3_client.return_value
+    mocked_instance.abort_multipart_upload.side_effect = exception
+
+    with pytest.raises(botocore.exceptions.ClientError):
+        s3_multipart_upload_abort(bucket_name=bucket, filename=location, upload_id=upload_id)
+
+    mocked_instance.abort_multipart_upload.assert_called_once()
