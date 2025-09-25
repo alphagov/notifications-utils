2afb5a5373e239f2b47522b596371c65e24f8be3
Pier-Angelo Gaetani <pgaetani@worth.systems>
2025-09-25 11:50:30 +0200

    fixes dutch address validation

---
+++ notifications_utils/recipient_validation/notifynl/postal_address.py
@@ -0,0 +1,65 @@
+import logging
+import re
+from functools import lru_cache
+
+from notifications_utils.countries import Country, CountryNotFoundError
+
+from ..postal_address import PostalAddress as PostalAddressUK
+from ..postal_address import normalise_postcode
+
+log = logging.getLogger(__name__)
+country_NL = Country("Netherlands")
+NL_POSTCODE_REGEX = r"\b[1-9][0-9]{3}\s?[A-Z]{2}\b"
+
+
+def _is_a_real_nl_postcode(postcode):
+    if not postcode:
+        return False
+
+    postcode = postcode.strip().upper()
+    return bool(re.fullmatch(NL_POSTCODE_REGEX, postcode.replace(" ", "")))
+
+
+def format_postcode_for_printing(postcode):
+    postcode = normalise_postcode(postcode)
+    return postcode[:4] + " " + postcode[4:]
+
+
+@lru_cache(maxsize=8)
+def format_postcode_or_none(postcode):
+    if _is_a_real_nl_postcode(postcode):
+        return format_postcode_for_printing(postcode)
+
+
+class PostalAddress(PostalAddressUK):
+    MIN_LINES = 2
+
+    def __init__(self, address: str | list, allow_international_letters: bool = True):
+        if isinstance(address, list):
+            address: str = "\n".join(address)
+
+        super().__init__(address, allow_international_letters)
+
+        try:
+            self.country = Country(self._lines_without_bfpo[-1])
+            self._lines_without_country_or_bfpo = self._lines_without_bfpo[:-1]
+        except CountryNotFoundError:
+            self._lines_without_country_or_bfpo = self._lines_without_bfpo
+            self.country = country_NL
+
+    @property
+    def postcode(self):
+        if not self._lines_without_country_or_bfpo:
+            log.warning("Postcode line not found")
+            return None
+
+        # scan lines in reverse
+        for line in reversed(self._lines_without_country_or_bfpo):
+            line_upper = line.upper()
+            match = re.search(NL_POSTCODE_REGEX, line_upper)
+            if match:
+                normalized = match.group(0).replace(" ", "")
+                return normalized[:4] + " " + normalized[4:]
+
+        log.warning("No valid NL postcode found in address lines: %s", self._lines_without_country_or_bfpo)
+        return None
