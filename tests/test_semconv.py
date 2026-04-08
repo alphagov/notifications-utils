7fc67b256b16752b1bf3bbe5c5c7eb286fb712db
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2026-04-08 17:57:49 +0100

    semconv: Add set_service_instance_id

--- tests/test_semconv.py
+++ tests/test_semconv.py
@@ -1,7 +1,10 @@
+import os
+
 import pytest
 from opentelemetry.util.types import AttributeValue
+from pytest_mock import MockerFixture
 
-from notifications_utils.semconv import set_error_type
+from notifications_utils.semconv import set_error_type, set_service_instance_id
 
 
 class MyException(Exception):
@@ -28,3 +31,16 @@ def test_set_error_type_with_exception() -> None:
                 "foo.bar": "baz",
                 "error.type": "tests.test_semconv.MyException",
             }
+
+
+def test_set_service_instance_id(mocker: MockerFixture) -> None:
+    mocker.patch("notifications_utils.semconv.uuid4", return_value="00000000-0000-0000-0000-000000000000")
+    set_service_instance_id()
+    assert os.environ["OTEL_RESOURCE_ATTRIBUTES"] == "service.instance.id=00000000-0000-0000-0000-000000000000"
+
+
+def test_set_service_instance_id_retains_existing_resource_attributes(mocker: MockerFixture) -> None:
+    mocker.patch("notifications_utils.semconv.uuid4", return_value="00000000-0000-0000-0000-000000000000")
+    os.environ["OTEL_RESOURCE_ATTRIBUTES"] = "foo=bar"
+    set_service_instance_id()
+    assert os.environ["OTEL_RESOURCE_ATTRIBUTES"] == "foo=bar,service.instance.id=00000000-0000-0000-0000-000000000000"
