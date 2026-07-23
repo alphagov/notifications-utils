f0b43ad7d1a35978c26cc39de89e1083c4abd9b2
Chris Hill-Scott <me@quis.cc>
2026-07-23 17:04:23 +0100

    Remove `RecipientCSV.rows_as_list`
    If you try to look at `RecipientCSV.rows_as_list` before
    `RecipientCSV.rows` has been accessed you will incorrectly see that
    the file has no rows. This commit makes it so you will get an
    `AttributeError` instead.
    
    Any code which needs to loop through the rows should instead loop over
    `RecipientCSV.rows` or the `RecipientCSV` instance itself.
    
    We still need to store the list, but we mark the attribute as internal
    with an underscore.

--- tests/test_recipient_csv.py
+++ tests/test_recipient_csv.py
@@ -261,7 +261,7 @@ def test_get_rows_only_iterates_over_file_once(mocker):
         next(rows)
 
     assert row_mock.call_count == 3
-    assert recipients.rows_as_list is None
+    assert not hasattr(recipients, "_rows_as_list")
 
 
 @pytest.mark.parametrize(
@@ -1475,10 +1475,10 @@ def test_errors_on_qr_codes_with_too_much_data():
 
     assert recipients.has_errors is True
     assert len(list(recipients.rows_with_errors)) == 1
-    assert recipients.rows_as_list[0].has_error is False
-    assert recipients.rows_as_list[0].qr_code_too_long is None
-    assert recipients.rows_as_list[1].has_error is True
-    assert isinstance(recipients.rows_as_list[1].qr_code_too_long, QrCodeTooLong)
+    assert recipients[0].has_error is False
+    assert recipients[0].qr_code_too_long is None
+    assert recipients[1].has_error is True
+    assert isinstance(recipients[1].qr_code_too_long, QrCodeTooLong)
 
 
 def test_column_headers_are_cached(mocker):
@@ -1545,3 +1545,20 @@ def test_cell_ignore_and_error_checking(mocker):
         call("Phone Number", "789"),
         call("Name", "C"),
     ]
+
+
+def test_rows_as_list_is_not_defined_on_init():
+    recipients = RecipientCSV(
+        """
+        Phone Number, Name, Foo, Bar
+        123,          A,    foo, bar
+        456,          B,    foo, bar
+        789,          C,    foo, bar
+        """,
+        template=_sample_template("sms"),
+    )
+    with pytest.raises(AttributeError):
+        recipients._rows_as_list  # noqa: B018
+
+    assert len(recipients.rows) == 3  # Causes recipients._rows_as_list to be defined
+    assert len(recipients._rows_as_list) == 3
