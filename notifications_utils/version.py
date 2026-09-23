1c92181015497227d24c4920583d420b1cb2281a
James Robinson <james.robinson2@dsit.gov.uk>
2026-09-23 17:43:31 +0100

    Minor version bump

--- notifications_utils/version.py
+++ notifications_utils/version.py
@@ -5,4 +5,4 @@
 # - `make version-minor` for new features
 # - `make version-patch` for bug fixes
 
-__version__ = "132.0.2"  # a3f515ca329ac59f1d67c8bd5bcc531e
+__version__ = "132.1.0"  # 9bf4e846eec7d4097eb7ebe31d3b03be
